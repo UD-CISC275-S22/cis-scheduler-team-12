7f0abc9e7df91043244b45ecd00463c7f2e07440
Jackson Leadlove <leadlove@udel.edu>
2022-04-06 23:33:49 -0400

    Introduced basic functions for plan maniupulation

---
+++ src/components/PlanEditor.tsx
@@ -0,0 +1,36 @@
+/* !!!!!!!!!DELETE THIS LATER eslint-disable @typescript-eslint/no-unused-vars DELETE THIS LATER!!!!!!!!!*/
+import React, { useState } from "react";
+import { Button, Container, Row, Col, Form } from "react-bootstrap";
+import { Plan } from "../interfaces/plan";
+import { Semester } from "../interfaces/semester";
+
+export function PlanEditor({
+    changeEditing,
+    plan,
+    editPlan,
+    deletePlan
+}: {
+    changeEditing: () => void;
+    plan: Plan;
+    editPlan: (id: string, newPlan: Plan) => void;
+    deletePlan: (id: string) => void;
+}): JSX.Element {
+    const [name, setName] = useState<string>(plan.name);
+
+    const [semesters, setSemesters] = useState<Semester[]>(plan.semesters);
+
+    function save() {
+        editPlan(plan.id, {
+            ...plan,
+            name: name,
+            semesters: semesters
+        });
+        changeEditing();
+    }
+
+    function cancel() {
+        changeEditing();
+    }
+
+    return <></>;
+}
---
+++ src/components/PlanView.tsx
@@ -0,0 +1,24 @@
+/* !!!!!!!!!DELETE THIS LATER eslint-disable @typescript-eslint/no-unused-vars DELETE THIS LATER!!!!!!!!!*/
+import React from "react";
+import { useState } from "react";
+// import { Container, Row, Col } from "react-bootstrap";
+import { Plan } from "../interfaces/plan";
+
+export function PlanView({
+    plan,
+    deletePlan,
+    editPlan
+}: {
+    plan: Plan;
+    deletePlan: (id: string) => void;
+    editPlan: (id: string, newPlan: Plan) => void;
+}): JSX.Element {
+    const [editing, setEditing] = useState<boolean>(false);
+
+    function changeEditing() {
+        // Should link to an "editing plan state" component somehow
+        setEditing(!editing);
+    }
+
+    return <></>;
+}
