53f568878fb19fb061de693df78f21cc3e284f47
Alex Trexler <alextrex@udel.edu>
2022-04-25 12:05:17 -0400

    added other course info to courselist component

--- src/components/CourseList.tsx
+++ src/components/CourseList.tsx
@@ -2,18 +2,32 @@ import React from "react";
 import { Course } from "../interfaces/course";
 
 export function CourseList({ courses }: { courses: Course[] }): JSX.Element {
+    function getEmoji(bool: boolean): string {
+        if (bool) {
+            return "✔️";
+        } else {
+            return "❌";
+        }
+    }
+
     return (
         <table width="500" className="Course-table">
             <tr>
                 <th>Course Code</th>
                 <th>Course Name</th>
                 <th>Credits</th>
+                <th>Prereqs</th>
+                <th>Completed</th>
+                <th>Required</th>
             </tr>
             {courses.map((course: Course) => (
                 <tr key={course.id}>
-                    <td width="20%">{course.code}</td>
-                    <td width="50%">{course.title}</td>
-                    <td width="30%">{course.credits}</td>
+                    <td>{course.code}</td>
+                    <td>{course.title}</td>
+                    <td>{course.credits}</td>
+                    <td>{course.prereqs.toString}</td>
+                    <td>{getEmoji(course.completed)}</td>
+                    <td>{getEmoji(course.required)}</td>
                 </tr>
             ))}
         </table>
