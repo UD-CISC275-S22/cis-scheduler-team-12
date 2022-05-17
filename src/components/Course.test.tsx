7351b1d00369803c1bafabf4d86877366bd85bf2
Andrew Woods <awoods@udel.edu>
2022-05-17 14:32:00 -0400

    finshed course tests

--- src/components/Course.test.tsx
+++ src/components/Course.test.tsx
@@ -36,6 +36,20 @@ describe("Edit, Add, and Remove Courses Tests", () => {
         //checking to see if default name is in...successful adding of course
         expect(screen.queryByText(/new course/i)).toBeInTheDocument();
     });
+    test("Renders the default plan", () => {
+        const f22 = screen.getAllByText(/Course Code/i);
+        const s23 = screen.getAllByText(/Course Name/i);
+        const w23 = screen.getAllByText(/Credits/i);
+        const u23 = screen.getAllByText(/Prereqs/i);
+        const x23 = screen.getAllByText(/Completed/i);
+        const y23 = screen.getAllByText(/Required/i);
+        expect(f22[0]).toBeInTheDocument();
+        expect(s23[0]).toBeInTheDocument();
+        expect(w23[0]).toBeInTheDocument();
+        expect(u23[0]).toBeInTheDocument();
+        expect(x23[0]).toBeInTheDocument();
+        expect(y23[0]).toBeInTheDocument();
+    });
     // test("tesing to see if courses are listed", () => {
     //     //checking to see if default fields are even listed
     //     const titles = screen.getAllByText(/Course Code/i);
